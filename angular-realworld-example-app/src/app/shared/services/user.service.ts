d7fadc9cb9084243d37fc656795c275ba94d0838
vemuez <vemuez@gmail.com>
2017-06-29 10:55:46 +0100

    one line rxjs

--- angular-realworld-example-app/src/app/shared/services/user.service.ts
+++ angular-realworld-example-app/src/app/shared/services/user.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { Observable, BehaviorSubject, ReplaySubject } from 'rxjs';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
